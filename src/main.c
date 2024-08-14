df4881d295e368b0e1d481959afd9d5e7c68ee1d
Peter Ban <bn.peter.hun@gmail.com>
2024-08-14 17:40:31 +0200

    First commit

---
+++ src/main.c
@@ -0,0 +1,16 @@
+//
+// Created by peter on 2024.08.12..
+//
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+    stdio_init_all();
+    printf("Hello, world!\n");
+    return 0;
+}
+
+
+
+
